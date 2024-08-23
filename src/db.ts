62516fff2a566fde83af14509f149b0d1bd51392
Alif Triadi <aliftriadi.gaming@gmail.com>
2024-08-23 22:37:29 +0700

    change(nullcheckingconfig): change null check to true

--- src/db.ts
+++ src/db.ts
@@ -4,6 +4,6 @@ import * as postgres from 'postgres';
 import * as schema from './schema';
 
 const connectionString = process.env.DATABASE_URL;
-const client = postgres(connectionString, { debug: true });
+const client = postgres(connectionString!, { debug: true });
 
 export const db = drizzle(client, { schema });
